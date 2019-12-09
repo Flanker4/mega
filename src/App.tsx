144458af1d2e0eccfd009d7a3368c5c445f97598
Andrii Boiko <aboiko>
2019-12-09 18:56:21 +0200

    update home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Router } from "react-router-dom";
 import { Main } from "./Components/Main";
@@ -12,21 +11,6 @@ const App: React.FC = () => {
       <div className="App">
         <HeaderContainer />
         <Main />
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     </Router>
   );
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import logo from "../logo.svg";
 
 export const Home = (props: any) => {
-  return <h1>Home</h1>;
+  return (
+    <header className="App-header">
+      <h1>Home</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
 };
